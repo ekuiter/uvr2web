57234c3773530fd2e8ab54e07cf6ec748164023b
Elias Kuiter <info@elias-kuiter.de>
2014-05-23 23:48:50 +0200

    Documentation link updated

--- php/include/Renderer.class.php
+++ php/include/Renderer.class.php
@@ -143,7 +143,7 @@ class Renderer {
    */
   private function get_meta_nav() {
     $this->add_link_extern(Loc::t('about'), 'http://elias-kuiter.de/projects/uvr2web/');
-    $this->add_link_extern(Loc::t('docs'), 'http://uvr2web.elias-kuiter.de/docs/');
+    $this->add_link_extern(Loc::t('docs'), 'http://ekuiter.github.io/uvr2web/');
   }
 
   /**
